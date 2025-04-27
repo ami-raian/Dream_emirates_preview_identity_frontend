d957a1934a667989ef4a8c6e6556bfcd371cbd2a
RaiAn-CSE <ami.robirai@gmail.com>
2025-04-27 12:56:10 +0600

    update on image shown in identity template

--- components/gold-trading-comp/header.tsx
+++ components/gold-trading-comp/header.tsx
@@ -10,6 +10,9 @@ export function Header({
   websiteName: string;
   logo: string;
 }) {
+  console.log({ websiteName, logo });
+  const imageUrl = `${process.env.NEXT_PUBLIC_API_URL2}/media-svc/image/get-presigned-url/${logo}`;
+  console.log({ imageUrl });
   return (
     <header className="container bg-[#12132d]/5 backdrop-blur-sm border-b rounded-2xl border border-white/10 shadow-[inset_0_0_1px_rgba(255,255,255,0.1)]">
       <div className="mx-auto px-8 py-6">
@@ -18,7 +21,8 @@ export function Header({
             {/* Logo */}
             <div className="w-16 h-16 relative rounded-full overflow-hidden bg-white/5 border border-white/10">
               <Image
-                src={logo || "/avatar-default.png"}
+                src={`${process.env.NEXT_PUBLIC_API_URL2}/media-svc/image/get-presigned-url/${logo}`}
+                // src={`https://staging.keycloak.dreamemirates.com/api/v1/media-svc/image/get-presigned-url/c6daf982-f7c7-41c4-bf75-3fdd99e39b6e.jpg`}
                 alt="Website Logo"
                 fill
                 className="object-contain"
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "staging.keycloak.dreamemirates.com",
+        pathname: "/api/v1/media-svc/image/get-presigned-url/**",
+      },
+    ],
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
